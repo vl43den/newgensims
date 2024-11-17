f3052b89311f2f8a35c0870f94bca8f6e9d655d9
Patrick Diem <patrick.diem@fhstp.ac.at>
2024-11-17 16:11:04 +0100

    WebApp

--- UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
+++ UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd32f3498512cc0f8717e89b5689fd3f6b547e53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee69486e0894fd6a23743a3298f7b642c650d5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,49 +1,25 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.EntityFrameworkCore;
-using WebApp.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-builder.Services.AddRazorComponents()
-    .AddInteractiveComponents();
-
-// Add Redis caching
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration.GetValue<string>("Redis:ConnectionString");
-    options.InstanceName = "WebApp_";
-});
-
-// Add other services
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-app.UseRouting();
+app.UseHttpsRedirection();
 
-app.MapRazorComponents()
-    .AddInteractiveComponents();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
