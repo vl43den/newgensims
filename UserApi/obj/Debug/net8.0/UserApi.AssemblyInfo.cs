6361c57c1a187f0056df78e320066a5a27b43dc2
vl43den <is231338@fhstp.ac.at>
2024-11-22 15:52:12 +0100

    Downgraded packages because of dependency errors

--- UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
+++ UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8731deb0aa9f111233d6f03e92e19cc294b6c98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42115ebd8e48ca06fc2609cb3661a32b6d8a6a27")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
