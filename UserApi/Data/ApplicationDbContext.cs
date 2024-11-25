c5b4580780d420578418051b86dfcce5fc46fa9a
Vladan Sekulic <is231338@fhstp.ac.at>
2024-11-25 20:07:50 +0100

    Update ApplicationDbContext.cs

--- UserApi/Data/ApplicationDbContext.cs
+++ UserApi/Data/ApplicationDbContext.cs
@@ -1,16 +1,20 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore; // Add this if not already present
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore; 
 using Microsoft.EntityFrameworkCore;
 using UserApi.Models;
 
 namespace UserApi.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<User>
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer(@"Data Source=localhost;User ID=sa;Password=YourNewStrongPassword123!;Database=UserApiDb;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
         public DbSet<User> Users { get; set; }
-        public DbSet<LogEntry> LogEntries { get; set; }  // Add this definition
     }
 }
